b0a25883b9ca6b0b47eeaa13fc46b11d7059879a
Anas Abuobaid <abuobaid.anas1999@gmail.com>
2022-06-13 13:45:19 +0300

    basic auth

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # basic-auth
+
+## UML Digrame
+
+![uml](./dev/Screenshot%20(93).png)
+
+## Links
+
+- [Heroku App](https://anas-basic-api-server.herokuapp.com/food)
+
+- [Pull Requset](https://github.com/abuobaid9/basic-auth/pulls)
+
+- [Actions](https://github.com/abuobaid9/basic-auth/actions/new)
+
+## Image
+
+![img1](./dev/Screenshot%20(90).png)
+![img2](./dev/Screenshot%20(91).png)
---
+++ src/routs/singup.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const {Users} =require('../models/index');
+const signUpRouter = express.Router();
+signUpRouter.post('/signup', async (req, res) => {
+
+    try {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+      const record = await Users.create(req.body);
+      res.status(201).json(record);
+    } catch (e) { res.status(403).send('Error Creating User'); }
+  });
+
+
+
+  module.exports = signUpRouter;
